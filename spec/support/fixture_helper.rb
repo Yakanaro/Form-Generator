761c704f7d21bb4f615a2f57d6838d68a85b7e89
Yakanaro <kipaherd@gmail.com>
2022-12-22 15:06:22 +0300

    fix

--- spec/support/fixture_helper.rb
+++ spec/support/fixture_helper.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module FixtureHelper
-  DIRECTORY = "spec/fixtures/files/forms/"
+  DIRECTORY = 'spec/fixtures/files/forms/'
   def read_fixture_file(filename)
     path = [DIRECTORY, filename].join
     html = File.read(path)
-    html.gsub("\n", "")
+    html.gsub("\n", '')
   end
 end
